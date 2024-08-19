aa3e9e03d40185dce2ecb80d0c552b4ef4a7f698
ByHeru04 <heru71787@gmail.com>
2024-08-19 13:23:14 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -194,8 +194,9 @@ async function getHelp(env: Env, url: URL): Promise<string> {
     <div class="status-container">
         <h1>CORS Proxy ByHeru🗿</h1>
         <p>Mau Ngapain Bang ?</p>
+        <p>User Telegram Admin : ByHeru </p>
         <div class="link-container">
-            <a href="https://cors.byheru-halamadrid.workers.dev/https://t.me/ByHeru" target="_blank">Telegram🗿</a>
+            <a href="https://cors.byheru-halamadrid.workers.dev/t.me/+6283803735374" target="_blank">Telegram🗿</a>
             <a href="https://cf-worker-ws-dev.byheru-halamadrid.workers.dev/HalaMadrid" target="_blank">Trojan🗿</a>
             <a href="https://cors-proxy.byheru-halamadrid.workers.dev/" target="_blank">Cors Proxy V2🗿</a>
             <a href="https://cors-proxy-test.byheru-halamadrid.workers.dev/" target="_blank">Cors Proxy (Disarankan)🗿</a>
