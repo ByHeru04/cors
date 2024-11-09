1d63a30824d3daba0b83a51f3a2f42a062d7389f
ByHeru04 <heru71787@gmail.com>
2024-11-09 19:17:35 +0700

    index.ts

--- src/index.ts
+++ src/index.ts
@@ -206,7 +206,7 @@ async function getHelp(env: Env, url: URL) {
             <a href="javascript:void(0)" onclick="changeServer('https://www.hlsplayer.net/embed?type=m3u8&src=https://test.byheru.workers.dev/ballbar_16240.m3u8')" class="server-button">S5</a>
             <a href="javascript:void(0)" onclick="changeServer('https://www.hlsplayer.net/embed?type=m3u8&src=https://test.byheru.workers.dev/ballbar_29830.m3u8')" class="server-button">S6</a>
             <a href="javascript:void(0)" onclick="changeServer('https://www.hlsplayer.net/embed?type=m3u8&src=https://test.byheru.workers.dev/ballbar_37150.m3u8')" class="server-button">S7</a>
-            <a href="javascript:void(0)" onclick="changeServer('https://cors-test.byheru.workers.dev/zeeplayer2.pages.dev/tv/bein3?id=bein3&type=m')" class="server-button">S8</a>
+            <a href="javascript:void(0)" onclick="changeServer('https://zeeplayer2.pages.dev/tv/bein3?id=bein3&type=m')" class="server-button">S8</a>
         </div>
     </div>
 
