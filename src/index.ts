17d69e194d845466c946bd9333921a24c3cd74da
ByHeru04 <heru71787@gmail.com>
2024-11-22 20:05:42 +0700

    index.ts

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,7 @@ export default {
       status: 200,
       text: "OK",
       headers: new Headers({
-        "Access-Control-Allow-Credentials": "true"
+        "Access-Control-Allow-Credentials": "true",
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods":
           "GET, POST, PUT, PATCH, DELETE, OPTIONS",
